ddaae6c7d1a03beb3d9ebfcb609baeb83bbd510f
Vladlina <92329114+Vladlina@users.noreply.github.com>
2024-06-09 14:00:08 +0200

    task 2 completed

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -5,14 +5,29 @@
 //Нумерація елементів має починатися з першого.
 //['Джаз', 'Блюз', 'Рок-н-рол']
 
-
-
 function logItems(array) {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];
-        console.log(`${i + 1} ${element}`)
-    }
-
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    console.log(`${i + 1} ${element}`);
+  }
 }
 
-logItems(['Джаз', 'Блюз', 'Рок-н-рол'])
+// logItems(['Джаз', 'Блюз', 'Рок-н-рол'])
+
+//Напиши функцію findSmallerNumber(numbers)
+//яка шукає найменше число в масиві.
+//Додай перевірку що функція отримує масив
+const numbers = [12, 5, 35, 56, 3, 24, 7, 80, 12, -1];
+
+function findSmallerNumber(numbers) {
+  let smallestNumber = numbers[0];
+
+  for (const number of numbers) {
+    if (number < smallestNumber) {
+      smallestNumber = number;
+    }
+  }
+  return smallestNumber;
+}
+
+console.log(findSmallerNumber(numbers));
